8e009e44684f21635d1ef3df3f0ea2c1ea9b5ae8
Ido Sagiv <ido.sagiv@mail.huji.ac.il>
2021-04-20 23:55:04 +0300

    remove old test

--- src/IndexReaderTest.java
+++ src/IndexReaderTest.java
@@ -1,39 +1,118 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.Console;
-import java.io.IOException;
-import java.util.*;
-
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import webdata.IndexReader;
 import webdata.SlowIndexWriter;
 
+import java.util.*;
 
-public class IndexReaderTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexReaderTest {
+    IndexReader indexReader;
 
     final static String DictionaryPath = "indexFiles";
     final static String DataSetPath = "datasets\\1000.txt";
 
-    @BeforeClass
+    @BeforeAll
     public static void prep() {
         SlowIndexWriter writer = new SlowIndexWriter();
         long startTime = System.currentTimeMillis();
         writer.slowWrite(DataSetPath, DictionaryPath);
         long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println("creating index in: "+ estimatedTime + " ms");
+        System.out.println("creating index in: " + estimatedTime + " ms");
+    }
+
+    @BeforeEach
+    void indexReader() {
+        this.indexReader = new IndexReader(DictionaryPath);
     }
 
     @Test
-    public void getProductReviewsfShouldReturnEmpty() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
+    void getProductId() {
+        assertEquals("B001E4KFG0", this.indexReader.getProductId(1));
+        assertEquals("B001FKQQDO", this.indexReader.getProductId(777));
+        assertEquals("B006F2NYI2", this.indexReader.getProductId(1000));
+        assertNull(this.indexReader.getProductId(1001));
+    }
 
-        Enumeration<Integer> r = ir.getProductReviews("B009HINRX9");
-        assertTrue("no reviews for B009HINRX9 should exist", !r.hasMoreElements());
+    @Test
+    void getReviewScore() {
+        assertEquals(5, this.indexReader.getReviewScore(1));
+        assertEquals(3, this.indexReader.getReviewScore(777));
+        assertEquals(2, this.indexReader.getReviewScore(1000));
+        assertEquals(-1, this.indexReader.getReviewScore(1001));
+    }
+
+    @Test
+    void getReviewHelpfulnessNumerator() {
+        assertEquals(1, this.indexReader.getReviewHelpfulnessNumerator(1));
+        assertEquals(0, this.indexReader.getReviewHelpfulnessNumerator(777));
+        assertEquals(2, this.indexReader.getReviewHelpfulnessNumerator(1000));
+        assertEquals(-1, this.indexReader.getReviewHelpfulnessNumerator(1001));
+    }
+
+    @Test
+    void getReviewHelpfulnessDenominator() {
+        assertEquals(1, this.indexReader.getReviewHelpfulnessDenominator(1));
+        assertEquals(0, this.indexReader.getReviewHelpfulnessDenominator(777));
+        assertEquals(5, this.indexReader.getReviewHelpfulnessDenominator(1000));
+        assertEquals(-1, this.indexReader.getReviewHelpfulnessNumerator(1001));
+    }
+
+    @Test
+    void getReviewLength() {
+        assertEquals(48, this.indexReader.getReviewLength(1));
+        assertEquals(82, this.indexReader.getReviewLength(777));
+        assertEquals(102, this.indexReader.getReviewLength(1000));
+        assertEquals(-1, this.indexReader.getReviewLength(1001));
+    }
+
+    @Test
+    void getTokenFrequency() {
+        assertEquals(1, this.indexReader.getTokenFrequency("snakes"));
+        assertEquals(3, this.indexReader.getTokenFrequency("raspberry"));
+        assertEquals(21, this.indexReader.getTokenFrequency("oatmeal"));
+        assertEquals(381, this.indexReader.getTokenFrequency("have"));
+    }
+
+    @Test
+    void getTokenCollectionFrequency() {
+        assertEquals(574, this.indexReader.getTokenCollectionFrequency("have"));
+        assertEquals(0, this.indexReader.getTokenCollectionFrequency("dorsagim"));
+    }
+
+    @Test
+    void getReviewsWithToken() {
+        assertTrue(Collections.list(this.indexReader.getReviewsWithToken("dorsagim")).isEmpty());
+    }
+
+    @Test
+    void getNumberOfReviews() {
+        assertEquals(1000, this.indexReader.getNumberOfReviews());
+    }
+
+    @Test
+    void getTokenSizeOfReviews() {
+        assertEquals(75447, this.indexReader.getTokenSizeOfReviews());
+    }
+
+    @Test
+    void getProductReviews() {
+        int i = 5;
+        Enumeration<Integer> x = this.indexReader.getProductReviews("B006K2ZZ7K");
+        assertEquals(4, Collections.list(x).size());
+        for (int key : Collections.list(x)) {
+            assertEquals(i++, key);
+        }
+        assertEquals(13, Collections.list(this.indexReader.getProductReviews("B006F2NYI2")).size());
+        assertEquals(0, Collections.list(this.indexReader.getProductReviews("XXXXXXXXXX")).size());
+    }
+
+    @Test
+    public void getProductReviewsfShouldReturnEmpty() {
+        Enumeration<Integer> r = this.indexReader.getProductReviews("B009HINRX9");
+        assertFalse(r.hasMoreElements(), "no reviews for B009HINRX9 should exist");
     }
 
 
@@ -42,8 +121,7 @@ public class IndexReaderTest {
     }
 
     @Test
-    public void ShouldFindCorrectIdiesFromProduct() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
+    public void ShouldFindCorrectIdiesFromProduct() {
         HashMap<String, List<Integer>> map = new HashMap<String, List<Integer>>() {{
             put("B001E4KFG0", Arrays.asList(1));
             put("B0009XLVG0", Arrays.asList(12, 13));
@@ -51,18 +129,17 @@ public class IndexReaderTest {
             put("B006F2NYI2", Arrays.asList(988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999, 1000));
         }};
 
-        for (String id : map.keySet()) {
-            List<Integer> actual = Collections.list(ir.getProductReviews(id));
-            List<Integer> expected = map.get(id);
-            Assert.assertTrue(GetErrorMSG(id, expected, actual), expected.equals(actual));
-        }
 
+        for (String id : map.keySet()) {
+            List<Integer> actual = Collections.list(this.indexReader.getProductReviews(id));
+            List<Integer> expected = map.get(id);
+            assertEquals(expected, actual, GetErrorMSG(id, expected, actual));
+        }
     }
 
 
     @Test
-    public void ShouldFindCorrectIdiesFromText() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
+    public void ShouldFindCorrectIdiesFromText() {
         HashMap<String, List<Integer>> map = new HashMap<String, List<Integer>>() {{
             put("endurolyte", Arrays.asList(64, 3));
             put("Vitality", Arrays.asList(1, 1));
@@ -73,44 +150,55 @@ public class IndexReaderTest {
             put("person", Arrays.asList(42, 1, 74, 1, 147, 1, 209, 1, 422, 1, 509, 1, 540, 1, 554, 1, 593, 2, 652, 1, 726, 1, 855, 1));
         }};
 
+        for (String text : map.keySet()) {
+            List<Integer> actual = Collections.list(this.indexReader.getReviewsWithToken(text));
+            List<Integer> expected = map.get(text);
+            assertEquals(expected, actual, GetErrorMSG(text, expected, actual));
+        }
+    }
+
+    @Test
+    public void ShouldFindCorrectIdiesFromText2() {
+        HashMap<String, List<Integer>> map = new HashMap<String, List<Integer>>() {{
+            put("0", Arrays.asList(41, 1, 130, 1, 159, 1, 596, 1, 746, 2, 776, 1, 863, 1, 930, 1));
+            put("09", Arrays.asList(966, 1));
+            put("0g", Arrays.asList(746, 1));
+            put("100ml", Arrays.asList(411, 1));
+            put("zip", Arrays.asList(17, 1, 193, 2, 476, 1, 690, 1, 852, 1));
+            put("zippy", Arrays.asList(627, 1));
+            put("zola", Arrays.asList(747, 1));
+            put("zucchini", Arrays.asList(902, 2, 932, 1, 942, 1, 944, 1));
+        }};
 
         for (String text : map.keySet()) {
-            List<Integer> actual = Collections.list(ir.getReviewsWithToken(text));
+            List<Integer> actual = Collections.list(this.indexReader.getReviewsWithToken(text));
             List<Integer> expected = map.get(text);
-            Assert.assertTrue(GetErrorMSG(text, expected, actual), expected.equals(actual));
+            assertEquals(expected, actual, GetErrorMSG(text, expected, actual));
         }
     }
 
     @Test
-    public void ShouldReturnEmptyForWordNotExisting() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
+    public void ShouldReturnEmptyForWordNotExisting() {
         List<String> map = Arrays.asList("jhskdf", "Vitality11");
 
         for (String text : map) {
-            List<Integer> actual = Collections.list(ir.getReviewsWithToken(text));
-            Assert.assertTrue(text + " should return empty enumeration", actual.isEmpty());
+            List<Integer> actual = Collections.list(this.indexReader.getReviewsWithToken(text));
+            assertTrue(actual.isEmpty(), text + " should return empty enumeration");
         }
-
     }
 
     @Test
-    public void WordCountShouldBeZero() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
+    public void WordCountShouldBeZero() {
         List<String> map = Arrays.asList("jhskdf", "Vitality11");
 
         for (String text : map) {
-            int actual = ir.getTokenCollectionFrequency(text);
-            Assert.assertEquals(actual, 0);
+            int actual = this.indexReader.getTokenCollectionFrequency(text);
+            assertEquals(actual, 0);
         }
-
     }
 
     @Test
-    public void GetCountTest() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
+    public void GetCountTest() {
         HashMap<String, Integer> map = new HashMap<String, Integer>() {{
             put("endurolyte", 3);
             put("Vitality", 1);
@@ -123,29 +211,42 @@ public class IndexReaderTest {
         }};
 
         for (String text : map.keySet()) {
-            int actual = ir.getTokenCollectionFrequency(text);
-            Assert.assertTrue(text + " should appear " + map.get(text) + " times and not " + actual, actual == map.get(text));
+            int actual = this.indexReader.getTokenCollectionFrequency(text);
+            assertEquals(map.get(text), actual, text + " should appear " + map.get(text) + " times and not " + actual);
         }
-
     }
 
     @Test
-    public void FrequencyShouldBe0() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
+    public void GetCountTest2() {
+        HashMap<String, Integer> map = new HashMap<String, Integer>() {{
+            put("0", 9);
+            put("09", 1);
+            put("0g", 1);
+            put("100ml", 1);
+            put("zip", 6);
+            put("zippy", 1);
+            put("zola", 1);
+            put("zucchini", 5);
+        }};
 
+        for (String text : map.keySet()) {
+            int actual = this.indexReader.getTokenCollectionFrequency(text);
+            assertEquals(map.get(text), actual, text + " should appear " + map.get(text) + " times and not " + actual);
+        }
+    }
+
+    @Test
+    public void FrequencyShouldBe0() {
         List<String> map = Arrays.asList("jhskdf", "Vitality11");
 
         for (String text : map) {
-            int actual = ir.getTokenFrequency(text);
-            Assert.assertEquals(actual, 0);
+            int actual = this.indexReader.getTokenFrequency(text);
+            assertEquals(actual, 0);
         }
-
     }
 
     @Test
-    public void GetFrequencyTest() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
+    public void GetFrequencyTest() {
         HashMap<String, Integer> map = new HashMap<String, Integer>() {{
             put("endurolyte", 1);
             put("Vitality", 1);
@@ -155,150 +256,28 @@ public class IndexReaderTest {
             put("finicky", 2);
         }};
 
+        for (String text : map.keySet()) {
+            int actual = this.indexReader.getTokenFrequency(text);
+            assertEquals(map.get(text), actual, text + " should appear " + map.get(text) + " times and not " + actual);
+        }
+    }
+
+    @Test
+    public void GetFrequencyTest2() {
+        HashMap<String, Integer> map = new HashMap<String, Integer>() {{
+            put("0", 8);
+            put("09", 1);
+            put("0g", 1);
+            put("100ml", 1);
+            put("zip", 5);
+            put("zippy", 1);
+            put("zola", 1);
+            put("zucchini", 4);
+        }};
 
         for (String text : map.keySet()) {
-            int actual = ir.getTokenFrequency(text);
-            assertTrue(text + " should appear " + map.get(text) + " times and not " + actual, actual == map.get(text));
-        }
-
-    }
-
-    @Test
-    public void getReviewHelpfulnessDenominator() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(2, 0);
-            put(3, 1);
-            put(101, 5);
-            put(102, 3);
-            put(1000, 5);
-            put(999, 2);
-        }};
-
-        for (int id : map.keySet()) {
-            int actual = ir.getReviewHelpfulnessDenominator(id);
-            Assert.assertTrue(id + " Helpfulness should be " + map.get(id) + " and not " + actual, actual == map.get(id));
-        }
-
-    }
-
-    @Test
-    public void ReviewHelpfulnessDenominatorShouldBeMinus1() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(1001, -1);
-            put(-1, -1);
-        }};
-
-
-        for (int id : map.keySet()) {
-            int actual = ir.getReviewHelpfulnessDenominator(id);
-            Assert.assertTrue(id + " Helpfulness should be " + map.get(id) + " and not " + actual, actual == map.get(id));
-        }
-
-    }
-
-    @Test
-    public void ReviewHelpfulnessNumeratorShouldBeMinus1() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(1001, -1);
-            put(-1, -1);
-        }};
-
-        for (int id : map.keySet()) {
-            int actual = ir.getReviewHelpfulnessNumerator(id);
-            Assert.assertTrue(id + " Helpfulness should be " + map.get(id) + " and not " + actual, actual == map.get(id));
-        }
-
-    }
-
-    @Test
-    public void getReviewHelpfulnessNumerator() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(2, 0);
-            put(3, 1);
-            put(101, 4);
-            put(102, 2);
-            put(1000, 2);
-            put(999, 1);
-        }};
-
-        for (int id : map.keySet()) {
-            int actual = ir.getReviewHelpfulnessNumerator(id);
-            Assert.assertTrue(id + " Helpfulness should be " + map.get(id) + " and not " + actual, actual == map.get(id));
-        }
-
-    }
-
-    @Test
-    public void ReviewScoreShouldBeMinus1() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(1001, -1);
-            put(-1, -1);
-        }};
-
-        for (int id : map.keySet()) {
-            int actual = ir.getReviewScore(id);
-            Assert.assertTrue(id + " Score should be " + map.get(id) + " and not " + actual, actual == map.get(id));
-        }
-
-    }
-
-    @Test
-    public void getReviewScoreNumerator() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(2, 1);
-            put(3, 4);
-            put(101, 5);
-            put(102, 4);
-            put(999, 1);
-            put(1000, 2);
-        }};
-
-
-        for (int id : map.keySet()) {
-            int actual = ir.getReviewScore(id);
-            Assert.assertTrue(id + " Score should be " + map.get(id) + " and not " + actual, actual == map.get(id));
-        }
-
-    }
-
-    @Test
-    public void ProductIDShouldBeNULL() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-
-        List<Integer> list = Arrays.asList(1001, -1);
-
-        for (int id : list) {
-            String actual = ir.getProductId(id);
-            Assert.assertTrue(" ProductID should be null for " + id, actual == null);
-        }
-
-    }
-
-    @Test
-    public void getProductID() throws IOException {
-        IndexReader ir = new IndexReader(DictionaryPath);
-        HashMap<Integer, String> map = new HashMap<Integer, String>() {{
-            put(2, "B00813GRG4");
-            put(3, "B000LQOCH0");
-            put(101, "B004K2IHUO");
-            put(102, "B004K2IHUO");
-            put(999, "B006F2NYI2");
-            put(1000, "B006F2NYI2");
-        }};
-
-        for (int id : map.keySet()) {
-            String actual = ir.getProductId(id);
-            Assert.assertTrue(id + " product id should be " + map.get(id) + " and not " + actual, actual.equals(map.get(id)));
+            int actual = this.indexReader.getTokenFrequency(text);
+            assertEquals(map.get(text), actual, text + " should appear " + map.get(text) + " times and not " + actual);
         }
     }
-
 }
